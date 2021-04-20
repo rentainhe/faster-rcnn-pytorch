1a7b3a6c1c2de13a860c1e600ada8d20707a296b
rentainhe <596106517@qq.com>
2021-04-20 15:29:27 +0800

    add faster_rcnn_vgg16

---
+++ model/faster_rcnn_vgg16.py
@@ -0,0 +1,11 @@
+from __future__ import  absolute_import
+import torch
+from torch import nn
+from torchvision.models import vgg16
+from torchvision.ops import RoIPool
+
+from model.region_proposal_network import RegionProposalNetwork
+from model.faster_rcnn import FasterRCNN
+from utils import array_tool as at
+from utils.config import opt
+
