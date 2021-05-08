0e7e17cec2644c4c3a842c375d59dc4d4d3d8baf
rentainhe <596106517@qq.com>
2021-05-08 20:26:34 +0800

    update eval_tool

--- utils/eval_tool.py
+++ utils/eval_tool.py
@@ -70,7 +70,14 @@ def eval_detection_voc(
         * **map** (*float*): The average of Average Precisions over classes.
 
     """
+    prec, rec = calc_detection_voc_prec_rec(
+        pred_bboxes, pred_labels, pred_scores,
+        gt_bboxes, gt_labels, gt_difficults,
+        iou_thresh=iou_thresh)
 
+    ap = calc_detection_voc_ap(prec, rec, use_07_metric=use_07_metric)
+
+    return {'ap': ap, 'map': np.nanmean(ap)}
 
 def calc_detection_voc_prec_rec(
         pred_bboxes, pred_labels, pred_scores, gt_bboxes, gt_labels,
@@ -156,4 +163,80 @@ def calc_detection_voc_prec_rec(
             gt_difficult = np.zeros(gt_bbox.shape[0], dtype=bool)
 
         # np.unique函数去除 一维数组或者列表 中 重复 的元素，并按元素 由小到大 返回一个新的 无元素重复 的元组或者列表
+        # 这里 np.unique() 后剩下的就是所有的类别, 一个从0到20的数组, 也就是类别标签
         for l in np.unique(np.concatenate((pred_label, gt_label)).astype(int)):
+            pred_mask_l = pred_label == l
+            pred_bbox_l = pred_bbox[pred_mask_l]
+            pred_score_l = pred_score[pred_mask_l]
+            # sort by score
+            order = pred_score_l.argsort()[::-1]
+            pred_bbox_l = pred_bbox_l[order]
+            pred_score_l = pred_score_l[order]
+
+            gt_mask_l = gt_label == l
+            gt_bbox_l = gt_bbox[gt_mask_l]
+            gt_difficult_l = gt_difficult[gt_mask_l]
+
+            n_pos[l] += np.logical_not(gt_difficult_l).sum()
+            score[l].extend(pred_score_l)
+
+            if len(pred_score_l) == 0:
+                continue
+            if len(gt_bbox_l) == 0:
+                match[l].extend((0,) * pred_bbox_l.shape[0])
+                continue
+
+                # VOC evaluation follows integer typed bounding boxes.
+                pred_bbox_l = pred_bbox_l.copy()
+                pred_bbox_l[:, 2:] += 1
+                gt_bbox_l = gt_bbox_l.copy()
+                gt_bbox_l[:, 2:] += 1
+
+                iou = bbox_iou(pred_bbox_l, gt_bbox_l)
+                gt_index = iou.argmax(axis=1)
+                # set -1 if there is no matching ground truth
+                gt_index[iou.max(axis=1) < iou_thresh] = -1
+                del iou
+
+                selec = np.zeros(gt_bbox_l.shape[0], dtype=bool)
+                for gt_idx in gt_index:
+                    if gt_idx >= 0:
+                        if gt_difficult_l[gt_idx]:
+                            match[l].append(-1)
+                        else:
+                            if not selec[gt_idx]:
+                                match[l].append(1)
+                            else:
+                                match[l].append(0)
+                        selec[gt_idx] = True
+                    else:
+                        match[l].append(0)
+
+        for iter_ in (
+                pred_bboxes, pred_labels, pred_scores,
+                gt_bboxes, gt_labels, gt_difficults):
+            if next(iter_, None) is not None:
+                raise ValueError('Length of input iterables need to be same.')
+
+        n_fg_class = max(n_pos.keys()) + 1
+        prec = [None] * n_fg_class
+        rec = [None] * n_fg_class
+
+        for l in n_pos.keys():
+            score_l = np.array(score[l])
+            match_l = np.array(match[l], dtype=np.int8)
+
+            order = score_l.argsort()[::-1]
+            match_l = match_l[order]
+
+            tp = np.cumsum(match_l == 1)
+            fp = np.cumsum(match_l == 0)
+
+            # If an element of fp + tp is 0,
+            # the corresponding element of prec[l] is nan.
+            prec[l] = tp / (fp + tp)
+            # If n_pos[l] is 0, rec[l] is None.
+            if n_pos[l] > 0:
+                rec[l] = tp / n_pos[l]
+
+        return prec, rec
